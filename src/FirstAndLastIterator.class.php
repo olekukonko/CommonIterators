900e2411725a8dfa4d99de097cdf6b7b3dd50f01
olekukonko <github.com@12>
2013-01-15 01:17:36 +0100

    Added comments

---
+++ src/FirstAndLastIterator.class.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Run;
+use CachingIterator ;
+/**
+ * Iterator that iterates over the first and last element of
+ * an Iterator
+ */
+class FirstAndLastIterator extends CachingIterator
+{
+	/**
+	 * 
+	 * @param Iterator $iterator
+	 */
+    public function __construct(Iterator $iterator) {
+        parent::__construct($iterator, self::TOSTRING_USE_KEY);
+    }
+ 
+    /**
+     * Iterator::next — Return the next element
+     *
+     * @see Iterator::next()
+     * @link http://php.net/manual/en/iterator.next.php
+     */    
+    public function next() {
+        while (parent::hasNext()) {
+            parent::next();
+        }
+    }
+}
+
+?>
